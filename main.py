dc27809234c85dd0094e4f2afbab91d5bfd9ad08
svinoczar <alexsupper75@gmail.com>
2024-09-18 17:51:32 +0300

    finish lab

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import janus_swi as pl 
 
 
-# Подключаем базу знаний
 pl.consult("lab_1.pl")
 
-
-# Словарь для блоков
 blocks = {
     'стекло': 'glass',
     'бетон': 'concrete',
@@ -37,8 +34,6 @@ blocks = {
     'лоза': 'vines',
     'папоротник': 'fern'
 }
-
-# Словарь для инструментов
 tools = {
     'рука': 'hand',
     'ножницы': 'shears',
@@ -49,33 +44,47 @@ tools = {
     'алмазная кирка': 'diamond_pickaxe',
     'незеритовая кирка': 'netherite_pickaxe',
 }
-
 get = ['получить', 'добыть', 'достать']
 wish = ['хочу', 'хочется', 'хотелось']
 ability = ['могу']
+name = ""
 
+
+def help():
+    string = '''
+    Вы можете вводить запросы следующего формата: 
+    "Я хочу добыть `блок`."
+    "У меня есть `инструмент`, что я могу добыть?"
+    
+    Также вы можете использовать следующие команды:
+    /exit - для выхода из программы
+    /help - для отображения этого сообщения
+    '''
+    print(string)
 
 
 def parse_input(user_input):
-    # Получаем интересы из ввода пользователя
-    parts = user_input.lower().split(", ")
-    name = parts[0].split(" ")[2].capitalize()
-    request = parts[-1]
-    return name, request
+    match user_input:
+        case '/exit':
+            exit(0)
+        case '/help':
+            help()
+    return user_input
     
 def recomendation(name, request):
-    wish_or_ability = 'wish' if any(w in request for w in wish) else 'ability'
+    wish_or_ability = 'wish' if any(w in request for w in wish) else 'ability'    
     get_action = [item for item in get if item in request]
     block = [key for key in blocks.keys() if key in request]
     tool = [key for key in tools.keys() if key in request]
     need = {'pickaxe': ['нужна', ', или любая кирка лучше этой'], 'shears': ['нужны', '']}
+
     
     match wish_or_ability:
         case 'wish':
-            if get_action and block:  # Проверяем, что нашли хотя бы один элемент
+            if get_action and block:  
                 tool_query, tools_found = pl.query(f"obtained_with_tool({blocks[block[0]]}, Tool)."), []
                 for result in tool_query:
-                    tools_found.append(result['Tool'])  # Добавляем найденный инструмент в список
+                    tools_found.append(result['Tool'])
                 if tools_found:
                     tool_name = list(tools.keys())[list(tools.values()).index(tools_found[0])]
                     confirmed_need = need[tools_found[0].split("_")[-1]]
@@ -84,31 +93,32 @@ def recomendation(name, request):
                     print(f"{name}, для {block[0]} нет подходящего инструмента.")
             else:
                 print(f"{name}, не найдены подходящие блоки или действия в вашем запросе.")
-                
-        case 'ability':
-            if get_action and (block or tool):  # Проверяем, что нашли хотя бы один элемент
-                tool_query, tools_found = pl.query(f"obtained_with_tool({blocks[block[0]]}, Tool)."), []
-                
-                for result in tool_query:
-                    tools_found.append(result['Tool'])  # Добавляем найденный инструмент в список
-                
-                if tools_found:
-                    tool_name = list(tools.keys())[list(tools.values()).index(tools_found[0])]
-                    confirmed_need = need[tools_found[0].split("_")[-1]]
-                    print(f"{name}, чтобы {get_action[0]} {block[0]}, тебе {confirmed_need[0]} {tool_name}{confirmed_need[1]}!")
 
+        case 'ability':
+            if tool:
+                tool_type = tools[tool[0]]
+                block_query = pl.query(f"obtained_with_tool(Block, '{tool_type}').")
+                if block_query:
+                    blocks_found = [result['Block'] for result in block_query]
+                    blocks_names = [key for key in blocks.keys() if blocks[key] in blocks_found]
+                    blocks_output = ", ".join(blocks_names)
+                    print(f"{name}, при помощи инструмента '{tool[0]}' ты можешь добыть: {blocks_output}!")
                 else:
-                    print(f"{name}, для {block[0]} нет подходящего инструмента.")
+                    print(f"{name}, при помощи инструмента '{tool[0]}' ничего нельзя добыть.")
             else:
-                print(f"{name}, не найдены подходящие блоки или действия в вашем запросе.")
+                print(f"{name}, не найден подходящий инструмент в вашем запросе.")
 
 
 
 def main():
-    print("Введите информацию о себе (например, 'Меня зовут Андрей, я хочу добыть алмазы!'):")
-    user_input = input("Ввод: ")
-    name, request = parse_input(user_input)
-    recomendation(name, request)
+    welcome_message = "Для начала давайте познакомимся! Как вас зовут?"
+    print(welcome_message)
+    name = input("Ввод: ").split(" ")[-1].capitalize() 
+    help()
+    while True:
+        user_input = input("Ввод: ")
+        request = parse_input(user_input)
+        if not request.startswith('/'): recomendation(name, request)
     
  
 if __name__ == "__main__":
